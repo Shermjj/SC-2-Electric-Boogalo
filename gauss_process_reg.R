b416e42961d5c297615323d1c115087a974b1581
NonDescriptMaths <kieranmorris77@gmail.com>
2024-05-27 17:15:34 +0100

    gauuss.cpp creatio

--- gauss_process_reg.R
+++ gauss_process_reg.R
@@ -1,4 +1,4 @@
-
+library(kernlab)
 
 irish_data <- readRDS("data/daily_agg_data.RDS")
 
@@ -7,11 +7,9 @@ print(head(irish_data))
 
 # Define a function which performs Gaussian process regression
 gaussian_process_reg <- function(data, kernel = "rbfdot",plot = FALSE) {
-  train_indices <- createDataPartition(data$demand, p = 0.7, list = FALSE)
-  train_set <- data[train_indices, ]
-
-# Use the remaining 30% for the test set
-  test_set <- data[-train_indices, ]
+  train_index <- round(nrow(data) * 0.7)
+  train_set <- data[1:train_indices, ]
+  test_set <- data[(train_index + 1):nrow(data), ]
 
   x = as.vector(train_set$dateTime)
   y = as.vector(train_set$demand)
@@ -38,3 +36,34 @@ gaussian_process_reg <- function(data, kernel = "rbfdot",plot = FALSE) {
 }
 
 #Define a function which performs gaussian process regression via Rcpp
+gauusian_process_reg_rcpp <- function(data, kernel = "rbfdot",plot = FALSE) {
+  sourceCpp("src/gauss_process_reg.cpp")
+  train_index <- round(nrow(data) * 0.7)
+  train_set <- data[1:train_indices, ]
+  test_set <- data[(train_index + 1):nrow(data), ]
+
+  x = as.vector(train_set$dateTime)
+  y = as.vector(train_set$demand)
+
+  # Define the Gaussian process model
+  gpr_model <- gauss_process_reg(x,y, kernel = kernel)
+  prediction <- data.frame(
+    dateTime = data$dateTime,
+    mean = predict(gpr_model, as.vector(data$dateTime)))
+  test_predictions <- predict(gpr_model,as.vector(test_set$dateTime))
+  performance <- postResample(pred = test_predictions, obs = test_set$demand)
+
+  data_predict <- left_join(data, prediction, by = "dateTime")
+  if (plot){
+    pl <- ggplot(data_predict, aes(x = dateTime)) +
+      geom_point(aes(y = demand), color = "#000000") +
+      geom_line(aes(y = mean), color = "red") +
+      labs(title = "GPR model predictions",
+           x = "Date", y = "Predicted demand")
+    
+  } else {
+    pl <- NULL
+  }
+  return(list(model = gpr_model, data = data_predict, pl = pl,performance= performance))
+}
+
---
+++ gauss_process_reg.cpp
@@ -0,0 +1,33 @@
+#include <Rcpp.h>
+#include <RcppArmadillo.h>
+
+// Rest of your code goes here
+// [[Rcpp::depends(RcppArmadillo)]]
+using namespace Rcpp;
+using namespace arma;
+
+// [[Rcpp::export]]
+
+List gauss_process_reg(arma::mat X, arma::vec y, arma::mat Xnew, double sigma2, double l) {
+  int n = X.n_rows;
+  int nnew = Xnew.n_rows;
+  mat K = exp(-1/(2*l*l)*square(X.each_row() - X.t()));
+  K.diag() += sigma2;
+  mat L = chol(K);
+  vec alpha = solve(trimatl(L), solve(trimatu(L), y));
+  mat Knew = exp(-1/(2*l*l)*square(X.each_row() - Xnew.t()));
+  vec mu = Knew.t() * alpha;
+  mat v = solve(trimatu(L), Knew);
+  mat cov = exp(-1/(2*l*l)*square(Xnew.each_row() - Xnew.t())) - v.t() * v;
+  return List::create(Named("mu") = mu, Named("cov") = cov);
+}
+
+/*** R
+ * X <- matrix(rnorm(100), 50, 2)
+ * y <- rnorm(50)
+ * Xnew <- matrix(rnorm(100), 50, 2)
+ * sigma2 <- 0.1
+ * l <- 1
+ * gauss_process_reg(X, y, Xnew, sigma2, l)
+ */
+
