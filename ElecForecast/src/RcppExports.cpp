e6797d8809335cdcf3ec02b553f8b52904074ba5
babichmorrowc <babichmorrowc@gmail.com>
2024-05-29 22:35:12 +0100

    Working version of ridge regression with cross-validation

--- ElecForecast/src/RcppExports.cpp
+++ ElecForecast/src/RcppExports.cpp
@@ -64,7 +64,7 @@ BEGIN_RCPP
 END_RCPP
 }
 // parallel_ridge_cross_validation
-NumericMatrix parallel_ridge_cross_validation(NumericMatrix x_vars, NumericVector y_var, NumericVector time_counter, double daily_period, double annual_period, int max_K, std::vector<double> lambda_values, int n_folds);
+List parallel_ridge_cross_validation(NumericMatrix x_vars, NumericVector y_var, NumericVector time_counter, double daily_period, double annual_period, int max_K, std::vector<double> lambda_values, int n_folds);
 RcppExport SEXP _ElecForecast_parallel_ridge_cross_validation(SEXP x_varsSEXP, SEXP y_varSEXP, SEXP time_counterSEXP, SEXP daily_periodSEXP, SEXP annual_periodSEXP, SEXP max_KSEXP, SEXP lambda_valuesSEXP, SEXP n_foldsSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
@@ -81,6 +81,21 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// predict_parallel_ridge_cv
+NumericVector predict_parallel_ridge_cv(List model, NumericMatrix x_test, NumericVector time_counter, double daily_period, double annual_period);
+RcppExport SEXP _ElecForecast_predict_parallel_ridge_cv(SEXP modelSEXP, SEXP x_testSEXP, SEXP time_counterSEXP, SEXP daily_periodSEXP, SEXP annual_periodSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< List >::type model(modelSEXP);
+    Rcpp::traits::input_parameter< NumericMatrix >::type x_test(x_testSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type time_counter(time_counterSEXP);
+    Rcpp::traits::input_parameter< double >::type daily_period(daily_periodSEXP);
+    Rcpp::traits::input_parameter< double >::type annual_period(annual_periodSEXP);
+    rcpp_result_gen = Rcpp::wrap(predict_parallel_ridge_cv(model, x_test, time_counter, daily_period, annual_period));
+    return rcpp_result_gen;
+END_RCPP
+}
 
 static const R_CallMethodDef CallEntries[] = {
     {"_ElecForecast_GenFT", (DL_FUNC) &_ElecForecast_GenFT, 3},
@@ -88,6 +103,7 @@ static const R_CallMethodDef CallEntries[] = {
     {"_ElecForecast_RidgeReg", (DL_FUNC) &_ElecForecast_RidgeReg, 3},
     {"_ElecForecast_RidgeRegPar", (DL_FUNC) &_ElecForecast_RidgeRegPar, 3},
     {"_ElecForecast_parallel_ridge_cross_validation", (DL_FUNC) &_ElecForecast_parallel_ridge_cross_validation, 8},
+    {"_ElecForecast_predict_parallel_ridge_cv", (DL_FUNC) &_ElecForecast_predict_parallel_ridge_cv, 5},
     {NULL, NULL, 0}
 };
 
